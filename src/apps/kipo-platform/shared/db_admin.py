cd9d03e7739bbd1bfa42259e16da28af8cb87f1a
Edgar Figueroa <edgar.figueroa.gtz@gmail.com>
2026-07-13 15:10:01 -0600

    fix db conection

--- src/apps/kipo-platform/shared/db_admin.py
+++ src/apps/kipo-platform/shared/db_admin.py
@@ -6,5 +6,7 @@ from psycopg2.extensions import connection
 def admin_connection() -> connection:
     url = os.environ["DATABASE_URL"]
     if "sslmode" not in url:
-        url += ("&" if "?" in url else "?") + "sslmode=disable"
+        is_local = "127.0.0.1" in url or "localhost" in url
+        default_sslmode = "disable" if is_local else "verify-full"
+        url += ("&" if "?" in url else "?") + f"sslmode={default_sslmode}"
     return psycopg2.connect(url)
